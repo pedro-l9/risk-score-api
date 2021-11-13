320b9453b94833fe5fe3edba8dce177c11c7f992
Pedro Lacerda <contato@placerda.dev>
2021-11-13 09:41:52 -0300

    adds the highIncomeRiskDeduction rule

---
+++ src/rules/highIncomeRiskDeduction.ts
@@ -0,0 +1,24 @@
+import { RiskScore, ClientInformation } from '../model';
+
+const HIGH_INCOME_THRESHOLD = 200000;
+
+export default function highIncomeRiskDeduction(
+  { income }: ClientInformation,
+  { disability, home, auto, life }: RiskScore
+): RiskScore {
+  if (income > HIGH_INCOME_THRESHOLD) {
+    return {
+      disability: disability === undefined ? undefined : disability - 1,
+      home: home === undefined ? undefined : home - 1,
+      auto: auto === undefined ? undefined : auto - 1,
+      life: life === undefined ? undefined : life - 1,
+    };
+  }
+
+  return {
+    disability,
+    home,
+    auto,
+    life,
+  };
+}
---
+++ src/rules/tests/highIncomeRiskDeduction.test.ts
@@ -0,0 +1,53 @@
+import { ClientInformation, RiskScore } from '../../model';
+import highIncomeRiskDeduction from '../highIncomeRiskDeduction';
+
+const INITIAL_RISK_SCORE: RiskScore = {
+  auto: 0,
+  disability: 0,
+  home: 0,
+  life: 0,
+};
+
+const HIGH_INCOME_CLIENT: ClientInformation = {
+  age: 26,
+  dependents: 0,
+  house: { ownership_status: 'owned' },
+  income: 201000,
+  marital_status: 'married',
+  risk_questions: [0, 0, 0],
+  vehicle: { year: 2020 },
+};
+describe('The highIncomeRiskDeduction rule', () => {
+  it('Should deduct 1 risk point from all lines of insurance for a client that has an income above 200k', () => {
+    const expectedRiskScore: RiskScore = {
+      auto: -1,
+      disability: -1,
+      home: -1,
+      life: -1,
+    };
+
+    const ruleResult = highIncomeRiskDeduction(
+      HIGH_INCOME_CLIENT,
+      INITIAL_RISK_SCORE
+    );
+
+    expect(ruleResult).toEqual(expectedRiskScore);
+  });
+
+  it('Should not attribute a score to an undefined value', () => {
+    const ruleResult = highIncomeRiskDeduction(HIGH_INCOME_CLIENT, {});
+
+    expect(ruleResult).toEqual({});
+  });
+
+  it('Should not change the score of a client that has an income bellow 200k', () => {
+    const lowIncomeClient = { ...HIGH_INCOME_CLIENT, income: 20000 };
+
+    const ruleResult = highIncomeRiskDeduction(
+      lowIncomeClient,
+      INITIAL_RISK_SCORE
+    );
+
+    expect(ruleResult).toEqual(INITIAL_RISK_SCORE);
+  });
+});
